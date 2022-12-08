9eaa0b50305a17e31e4f4f7140e39d3bc0cddd27
dhruvi-2003-0 <115672700+dhruvi-2003-0@users.noreply.github.com>
2022-12-08 13:39:37 -0500

    Add files via upload

---
+++ menu.js
@@ -0,0 +1,26 @@
+
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
+
+const scrollline = document.querySelector('.scroll-line');
+
+      function fillscrollline(){
+        const windowHeight = window.innerHeight;
+        const fullHeight = document.body.clientHeight;
+        const scrolled = window.scrollY;
+        const percentScrolled = (scrolled / (fullHeight - windowHeight)) * 100;
+
+        scrollline.style.width = percentScrolled + '%';
+      };
+
+      window.addEventListener('scroll', fillscrollline);
+
+      
